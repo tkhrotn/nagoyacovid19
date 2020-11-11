b44dbb0a4399886c9fff031387ca7205f4165ddd
Takahiro Otani <tkhrotn@gmail.com>
2020-11-11 15:15:11 +0900

    2020年11月10日更新。

--- makeNagoyaCOVIDCSV.R
+++ makeNagoyaCOVIDCSV.R
@@ -7,5 +7,6 @@ for (f in csv) {
 }
 
 nagoyacovid <- nagoyacovid[order(nagoyacovid$名古屋市No),]
+nagoyacovid <- nagoyacovid[nagoyacovid$名古屋市No != 1180 & nagoyacovid$名古屋市No != 621 & nagoyacovid$名古屋市No != 925 & nagoyacovid$名古屋市No != 1435,]
 
 write.csv(nagoyacovid, "nagoyacovid.csv")
